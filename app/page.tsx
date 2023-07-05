3cffb12f1b287986a450920769e15bff0848ec90
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-07-06 02:11:36 +0500

    home page started

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import CityPicker from "@/components/CityPicker";
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+
+      <Card className="max-w-4xl mx-auto bg-white rounded-2xl">
+
+        <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
+        
+        <Subtitle className="text-xl text-center">Powered by OpenAI</Subtitle>
+
+        <Divider className="my-10 bg-black" />
+
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+          <CityPicker />
+        </Card>
+      
+      </Card>
+
+    </div>
+  )
+}
