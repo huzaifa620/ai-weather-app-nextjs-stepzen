77fd2a92067e8acd1e3fc9435a81c65bb3214a86
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-07-08 00:57:19 +0500

    select box added

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+'use client'
+
+import { Country, City } from "country-state-city"
+import { useState } from "react"
+import Select from "react-select"
+
+type option = {
+  value: {
+    latitude: string;
+    longitude: string;
+    isoCode: string;
+  };
+  label: string;
+} | null;
+
+type cityOption = {
+  value: {
+    latitude: string;
+    longitude: string;
+    countryCode: string;
+    name: string;
+    stateCode: string;
+  };
+  label: string;
+} | null;
+
+const options = Country.getAllCountries().map((country) => ({
+  value: {
+    latitude: country.latitude,
+    longitude: country.longitude,
+    isoCode: country.isoCode
+  },
+  label: country.name
+}))
 
 const CityPicker = () => {
+
+  const [selectedCountry, setSelectedCountry] = useState<option>(null)
+  const [selectedCity, setSelectedCity] = useState<cityOption>(null)
+
   return (
-    <div>CityPicker</div>
+    <div>
+      <Select options={options} />
+    </div>
   )
 }
 
