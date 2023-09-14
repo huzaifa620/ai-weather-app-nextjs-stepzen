46a0cce49813721ca125ac3b4c7077c21c2222b9
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-09-15 03:19:14 +0500

    Update page.tsx

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -36,7 +36,7 @@ const WeatherPage = async ({ params: { city, lat, long } }: Props) => {
 
   const dataToSend = cleanData(results, city)
 
-  const res = await fetch(`${process.env.VERCEL_URL}/api/getWeatherSummary`, {
+  const res = await fetch(`http://${process.env.VERCEL_URL}/api/getWeatherSummary`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
